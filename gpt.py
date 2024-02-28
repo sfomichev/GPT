90d2ab11cca54828746906707caee318b0b49984
sfomichev <sfomichev@gmail.com>
2024-02-29 01:47:04 +0300

    gpt

--- gpt.py
+++ gpt.py
@@ -192,7 +192,8 @@ class GPTLanguageModel(torch.nn.Module):
             idx = torch.cat((idx, next_id), dim = 1)     
         return idx
     
-#
+
+
 
 model = GPTLanguageModel()
 model = model.to(device)
@@ -211,7 +212,7 @@ for iter in range(max_iters):
     loss.backward()
     optimizer.step()
 
- 
+
 context = torch.zeros((1,block_size),dtype=torch.long, device=device)
 print(''.join(decode(model.generate(idx=context ,max_new_tokens=1000)[0].tolist())))
 
